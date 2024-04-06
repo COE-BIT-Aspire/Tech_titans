432ebc0ebd29c93242c5ce0ec955fda88cf6d490
SUJEETH-K <sujeeth4702@gmail.com>
2024-04-06 09:54:04 +0530

    New

---
+++ Sujeeth/SELF_EXPLORATION/sample.js
@@ -0,0 +1,65 @@
+// class Employee{
+//     empId: number;
+//     empName: string;
+//     constructor(){
+//         this.empId = 1;
+//         this.empName = "sujeeth";
+//     }
+//     display(){
+//         console.log(this.empId + " " + this.empName);
+//     }
+// }
+// let Employee1 = new Employee();
+// Employee1.display();
+//var sam = ["ana", 3, "one", 3.23, true];
+// class Employee{
+//     empId: number;
+//     empName: string;
+//     constructor(empId: number, empName: string){
+//         this.empId = empId;
+//         this.empName = empName;
+//     }
+//     show() {
+//         console.log(this.empId + " " + this.empName);
+//     }
+// }
+// class Manager extends Employee{
+//     constructor(empid: number, empname: string){
+//         super(empid, empname);
+//     }
+// }
+// let man1 = new Manager(2, "kavin");
+// man1.show();
+//Spread operator
+// let arr1 = [ 16,1,14,34];
+// let arr2 = [ 42, 53, 66,89];
+// let copy_arr = [...arr1];
+// let new_arr = [...arr1, 72, 83,78];
+// let merged_arr = [...arr1, ...arr2];
+// console.log(new_arr);
+function sum() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (acc, curr) { return acc + curr; }, 0);
+}
+console.log(sum(1, 2, 3, 4));
+// let set = new Set();
+// set.add("2");
+// set.add(true);
+// set.add("kavin");
+// set.add("kavin");
+// let map1 = new Map();
+// map1.set("ind", "India");
+// map1.set("in", "indonesia");
+//map1.get();
+// console.log(map1.keys());
+// let sam=function(){
+//     console.log("hello");
+// }
+// let sam1 = () => {
+//     console.log("hello");
+// }
+// let n: string = "kavin";
+// console.log(typeof(n));
