06e19ba322e682fed71d27c8aff29074313e79d9
PRIYADHARSHINI V <109613793+dharshini0510@users.noreply.github.com>
2024-05-03 07:40:31 +0530

    Add files via upload

---
+++ PRIYADHARSHINI V/PRIYADHARSHINI V/IMPACT TRAINING/JAVASCRIPT/ARRAYS/new file.js
@@ -0,0 +1,49 @@
+const numbers = [34,65,12,78]
+
+//sort
+//console.log(numbers.sort())
+
+
+//find
+/*let result = numbers.find((value)=>{
+    return value>50;
+});
+console.log(result)*/                  //prints only one value
+
+
+
+//filter
+/*let result = numbers.filter((value)=>{
+    return value>50;
+});
+console.log(result)*/               //prints all the values
+
+
+
+//map
+/*let result = numbers.map((value)=>{
+    return value>50;
+})
+console.log(result)*/               //boolean type answer
+
+/*let result = numbers.map((value)=>{
+    return '#'+ value;
+})
+console.log(result)*/ 
+
+/*let result = numbers.map((acc,value)=>{
+    return acc*value;
+})
+console.log(result)*/
+
+
+//reduce
+/*let result = numbers.reduce((value)=>{
+    return '#'+ value;
+})
+console.log(result)*/ 
+
+/*let result = numbers.reduce((acc,value)=>{
+    return acc*value;
+})
+console.log(result)*/
