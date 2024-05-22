31a4ebfa9976bb4be2fd34967cf1fb4336d260ac
SUJEETH-K <sujeeth4702@gmail.com>
2024-05-22 15:01:41 +0530

    changes

--- Angular/HotelCenter/src/app/app.component.ts
+++ Angular/HotelCenter/src/app/app.component.ts
@@ -7,11 +7,13 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { AcountHolderComponent } from './acount-holder/acount-holder.component';
 import { RouterLink } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,StudetComponent,FormsModule,ShopComponent,CommonModule,ProductComponent,AcountHolderComponent,RouterLink],
+  imports: [RouterOutlet,StudetComponent,FormsModule,ShopComponent,CommonModule,ProductComponent,AcountHolderComponent,RouterLink,ProductDetailsComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
