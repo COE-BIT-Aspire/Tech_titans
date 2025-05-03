154a68595cc65f127a586d9e05ccb083476209eb
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2025-05-03 20:58:29 +0530

    Delete PRIYADHARSHINI V directory

--- PRIYADHARSHINI V/PRIYADHARSHINI V/IMPACT TRAINING/JAVASCRIPT/ARRAYS/find element in an array.js
+++
@@ -1,9 +0,0 @@
-const products= ["Laptop", "Mobile", "Computer"]
-
-console.log(products.includes("Mobile"))            //boolean type answer
-
-//to find index
-console.log(products.indexOf("Computer"))
-console.log(products.lastIndexOf("Mobile"))
-
-console.log(products.indexOf("Laptop")!==1)
--- PRIYADHARSHINI V/PRIYADHARSHINI V/IMPACT TRAINING/JAVASCRIPT/FUNCTIONS/IIFE function.js
+++
@@ -1,7 +0,0 @@
-//Immediately Invoke Function Expression
-//created and executed then and there and it is not able to call later.
-
-( function iifeFunction() {
-    console.log('IIFE Function Executed');
-} ) ();
-
